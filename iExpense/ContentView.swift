f35ba99acc79d696e45e4f5e622a8e97c5a473b1
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-24 20:57:17 +0530

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Santhosh Srinivas on 24/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Santhosh Srinivas on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
