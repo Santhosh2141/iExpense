5d1d06b2d2d21753ea8bded6fb898f3abd1d18af
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-25 21:27:40 +0530

    storing user values when closed

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -35,23 +35,65 @@ struct ContentView: View {
     @StateObject private var user = User()
     
     @State private var showingSheet = false
+    @State private var num = [Int]()
+    @State private var currentNum = 1
+    @State private var buttonCount = UserDefaults.standard.integer(forKey: "Tap")
+    
+    @AppStorage("tapCount") private var tapCount = 0
+    // this is an easier way than UserDefaults. the value in quotes can be anything. not necessary that its the name of the var.
+    //    This works like @State: when the value changes, it will reinvoked the body property so our UI reflects the new data.
+    //    right now at least @AppStorage doesn’t make it easy to handle storing complex objects such as Swift structs
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, \(user.firstName) \(user.lastName)")
-            TextField("First Name", text: $user.firstName)
-            TextField("Last Name", text: $user.lastName)
-            Button("Show Sheet"){
-                showingSheet.toggle()
+        NavigationStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, \(user.firstName) \(user.lastName)")
+                TextField("First Name", text: $user.firstName)
+                TextField("Last Name", text: $user.lastName)
+                Button("Show Sheet"){
+                    showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet){
+                    SecondView(name: user.firstName)
+                }
+                List{
+                    ForEach(num, id: \.self){
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                    // onDelete works only on forEach we cant use it directly on the list
+                    Button("Button Count: \(buttonCount)"){
+                        buttonCount += 1
+                        UserDefaults.standard.set(buttonCount, forKey: "Tap")
+                        // this is used so that the value we use is saved when we quit app and reopen
+                    }
+                    Button("Tap Count: \(tapCount)"){
+                        tapCount += 1
+                    }
+                }
+                
+                Button("Add New number"){
+                    num.append(currentNum)
+                    currentNum += 1
+                }
+                
+                
             }
-            .sheet(isPresented: $showingSheet){
-                SecondView(name: user.firstName)
+            .padding()
+            .toolbar{
+                EditButton()
             }
         }
-        .padding()
+    }
+    
+    func removeRows(at offsets: IndexSet){
+//        we need to implement a method that will receive a single parameter of type IndexSet. This is a bit like a set of integers, except it’s sorted, and it’s just telling us the positions of all the items in the ForEach that should be removed.
+//
+//        Because our ForEach was created entirely from a single array, we can actually just pass that index set straight to our numbers array – it has a special remove(atOffsets:) method that accepts an index set.
+        num.remove(atOffsets: offsets)
     }
 }
 
